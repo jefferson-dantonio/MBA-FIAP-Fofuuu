f38985baa273a024ece3ffd1f95aedeafe3fe2b4
Jefferson D'Antonio Malaquias <jefferson.dantonio@gmail.com>
2021-07-21 22:28:54 -0300

    fixed encrypt

--- src/config/options/production.js
+++ src/config/options/production.js
@@ -7,7 +7,7 @@ module.exports = {
             password: process.env.APP_DB_PASSWORD,
             server: process.env.APP_DB_HOST,
             option:{
-                encrypt: "true"
+                encrypt: true
             }
         },
         pool: {
