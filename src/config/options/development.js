93d860b15d713d232b3073d920d1225ddb73934d
Alan <alan_brasilino@icloud.com>
2021-07-22 00:41:26 -0300

    Set requestTimeout and add encrypt

--- src/config/options/development.js
+++ src/config/options/development.js
@@ -3,14 +3,15 @@ module.exports = {
         client: 'mssql',
         connection: {
             database: process.env.APP_DB_NAME,
-            user: process.env.APP_DB_USER,
+            encrypt: true,
             password: process.env.APP_DB_PASSWORD,
             server: process.env.APP_DB_HOST,
+            user: process.env.APP_DB_USER,
+            requestTimeout: 100000
         },
         pool: {
             min: 2,
             max: 10
-        },
-        acquireConnectionTimeout: 30000
+        }
     }
 }
